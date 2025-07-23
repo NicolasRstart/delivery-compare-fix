f2aa96022144625087e96a01a65ce024ba13cdc9
NicolasRstart <nicoruocco2006@gmail.com>
2025-07-23 20:06:53 +0000

    adicion de consol log

--- config.js/script.js
+++ config.js/script.js
@@ -11,4 +11,29 @@ function cambiarSlide(direccion) {
   mostrarSlide(index + direccion);
 }
 
+  {
+    id: 1,
+    usuario; "Agustin Ruocco",
+    sitio; "Burger King Obelisco (PedidosYa)",
+    url; "https://www.pedidosya.com.ar/restaurantes/buenos-aires/burger-king-obelisco-a4cad619-85df-4ee4-9107-70ef92f21f8d-menu?search=burger+king",
+    estado; "no ingresado"
+  }
+  {
+    id: 2,
+    usuario; "Martín Sabatte",
+    sitio; "Mi Gusto Empanadas (Rappi)",
+    url; "https://www.rappi.com.ar/search?query=mi%20gusto%20empanadas",
+    estado; "no ingresado"
+  }
+  {
+    id: 3,
+    usuario; "Helsa Cosentino",
+    sitio; "Lucciano's Obelisco (PedidosYa)",
+    url; "https://www.pedidosya.com.ar/restaurantes/buenos-aires/luccianos-obelisco-4c7ffefd-b510-4bc7-8cf3-99b8fde44104-menu?search=helado",
+    estado; "no ingresado"
+  }
+
 mostrarSlide(index);
+
+//esto es el a lo que em referia lo modifique con algunas modificaciones al slider pero 
+// lo implemente en lo que tenia planeado con logs de donde fueron tres usuarios
